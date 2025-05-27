983149bc6938058f7673fb8bf8103762c512d397
sdj3959 <dj9099@naver.com>
2025-05-27 10:55:26 +0900

    변수와 데이터 타입 - 숫자

---
+++ 1-3-data-type/1-basic-type.js
@@ -0,0 +1,12 @@
+
+let element = 65; // number
+element = 'A'; // ==65 // string
+
+// 자바스크립트는 정수와 실수를 모두 실수로 판단한다.
+let age = 20;
+console.log(typeof age);
+
+// Infinity
+console.log(age / 0);
+// NaN : Not a Numer
+console.log(age * '메롱');
