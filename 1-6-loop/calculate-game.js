31ff95d5766692c487edb88c2f99fc33c8a523dc
sdj3959 <dj9099@naver.com>
2025-06-01 05:50:13 +0900

    1.2ver 요구사항 개발

--- 1-6-loop/calculate-game.js
+++ 1-6-loop/calculate-game.js
@@ -4,8 +4,16 @@ let questionNum = 1;
 let countCorrect = 0;
 let countWrong = 0;
 
+let calculateVer = 0;
+
 while (true) {
-  userAnswer = +prompt(`Q${questionNum}. ${rn1} + ${rn2} = ???`);
+  calculateVer = Math.floor(Math.random() * 3)+1;
+
+  if (calculateVer === 1) userAnswer = +prompt(`Q${questionNum}. ${rn1} + ${rn2} = ???`);
+  else if (calculateVer === 2) userAnswer = +prompt(`Q${questionNum}. ${rn1} - ${rn2} = ???`);
+  else if (calculateVer === 3) userAnswer = +prompt(`Q${questionNum}. ${rn1} * ${rn2} = ???`);
+
+
 
 
   if (userAnswer === 0) {
@@ -14,20 +22,31 @@ while (true) {
     countCorrect = 0;
     countWrong = 0;
     break;
-  } else if (userAnswer !== rn1 + rn2) {
-    rn1 = Math.floor(Math.random() * 10) + 1;
-    rn2 = Math.floor(Math.random() * 10) + 1;
-    questionNum++;
-    countWrong++;
-    alert(`틀렸습니다!`);
-    continue;
-  }
-  else {
+  } else if (userAnswer === rn1 + rn2 && calculateVer === 1) {
     alert(`정답입니다!`);
     rn1 = Math.floor(Math.random() * 10) + 1;
     rn2 = Math.floor(Math.random() * 10) + 1;
     questionNum++;
     countCorrect++;
+    continue;
+  } else if(userAnswer === rn1 - rn2 && calculateVer === 2){
+    alert(`정답입니다!`);
+    rn1 = Math.floor(Math.random() * 10) + 1;
+    rn2 = Math.floor(Math.random() * 10) + 1;
+    questionNum++;
+    countCorrect++;
+  } else if(userAnswer === rn1 * rn2 && calculateVer === 3){
+    alert(`정답입니다!`);
+    rn1 = Math.floor(Math.random() * 10) + 1;
+    rn2 = Math.floor(Math.random() * 10) + 1;
+    questionNum++;
+    countCorrect++;
+  } else {
+    rn1 = Math.floor(Math.random() * 10) + 1;
+    rn2 = Math.floor(Math.random() * 10) + 1;
+    questionNum++;
+    countWrong++;
+    alert(`틀렸습니다!`);
     continue;
   }
 }
