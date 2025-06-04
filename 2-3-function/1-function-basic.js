431508f16f47ce87349afb8a288f5c24b6ef0804
sdj3959 <dj9099@naver.com>
2025-06-04 09:50:54 +0900

    함수기초

---
+++ 2-3-function/1-function-basic.js
@@ -0,0 +1,12 @@
+
+// 함수 정의 : 함수를 만드는 과정 - 코드가 실행되지 않습니다.
+function sayHello(username) {
+  console.log(`${username}님 환영합니다`);
+  console.log(`오늘도 즐거운 하루 되세요!!`);
+}
+
+
+// 함수 호출 : 함수를 이제 실행시키는 과정
+sayHello('홍길동');
+sayHello('티니핑');
+
